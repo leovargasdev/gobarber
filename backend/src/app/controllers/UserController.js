9ca83576a96b5657b1fc4c9c3dbf8e670040af2f
Leonardo Vargas <leu1607@hotmail.com>
2020-02-04 23:40:53 -0300

    Adicionando o método para fazer o update das informações do usuário

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -15,6 +15,33 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    // Verificação 1: Caso tente alterar o email, verificar se o email já existe no banco
+    if (email && email !== user.email) {
+      const UserExists = await User.findOne({ where: { email } });
+
+      if (UserExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    // Verificação 2: Fornecer a senha antiga para poder alterar a senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { name, id, email: emailU, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email: emailU,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
