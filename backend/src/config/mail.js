d16ae6da012a7c20486f461edc2f0980a1a7989e
Leonardo Vargas <leu1607@hotmail.com>
2020-02-09 17:17:48 -0300

    Adicionando o nodemailer e configurando ao servidor do mailtrap, criando views para os templates de email

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bbd67c88a3e1bc',
+    pass: '6915b048928350',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com.br>',
+  },
+};
