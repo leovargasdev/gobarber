1f65361f2be62cb83902107bc780a2e61f003192
Leonardo Vargas <leu1607@hotmail.com>
2020-02-24 17:46:49 -0300

    Configurações do redux saga

---
+++ frontend/src/store/modules/auth/sagas.js
@@ -0,0 +1,66 @@
+import { all, call, takeLatest, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import history from '~/services/history';
+import api from '~/services/api';
+import { signInSuccess, signFailure } from './actions';
+
+export function* singIn({ payload }) {
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { user, token } = response.data;
+
+    if (!user.provider) {
+      toast.error('Usuários não é prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação');
+    yield put(signFailure());
+  }
+}
+
+export function* singUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    toast.success('Cadastro criado com sucesso!');
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha ao criar o cadastro!');
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function singOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', singIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', singUp),
+  takeLatest('@auth/SIGN_OUT', singOut),
+]);
---
+++ frontend/src/store/modules/user/sagas.js
@@ -0,0 +1,29 @@
+import { all, call, takeLatest, put } from 'redux-saga/effects';
+// import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    // toast.success('Perfil atualizado com sucesso!!!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    // toast.error('Falha ao atualizar perfil!!!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
