d3cece77efbacdbc227ce34b9410695a9166a2f0
Leonardo Vargas <leu1607@hotmail.com>
2020-02-25 15:48:12 -0300

    Finalizando a parte do Dashboard com a listagem dos agendamentos

--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
+
   /* Deixa todos os itens um abaixo do outro */
   display: flex;
   flex-direction: column;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,7 +5,7 @@ import Route from './Route';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
-// import Profile from '~/pages/Profile';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
@@ -13,7 +13,7 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      {/* <Route path="/profile" component={Profile} isPrivate /> */}
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
