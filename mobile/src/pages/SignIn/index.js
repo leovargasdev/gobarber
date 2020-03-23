c0296b584665485ec88703f7ecc2ec1eeb92908c
Leonardo Vargas <leu1607@hotmail.com>
2020-03-23 11:31:05 -0300

    Configurando o Redux, reactoron e criando o evento de login

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Image} from 'react-native';
+import {useDispatch} from 'react-redux';
 import logo from '~/assets/logo.png';
-import Background from '~/components/Background';
 
+import Background from '~/components/Background';
+import {signInRequest} from '~/store/modules/auth/actions';
 import {
   Container,
   Form,
@@ -13,6 +15,16 @@ import {
 } from './styles';
 
 export default function SignIn({navigation}) {
+  const dispatch = useDispatch();
+  const passwordRef = useRef(null);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <Background>
       <Container>
@@ -24,13 +36,22 @@ export default function SignIn({navigation}) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
-          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignText>Criar conta gratuita</SignText>
--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Image} from 'react-native';
 import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-import {Container, Form, FormInput, SubmitButton} from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignText,
+} from './styles';
 
 export default function SignUp({navigation}) {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -16,6 +28,8 @@ export default function SignUp({navigation}) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Nome Completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
           />
           <FormInput
             icon="mail-outline"
@@ -23,14 +37,23 @@ export default function SignUp({navigation}) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
-          <SubmitButton onPress={() => {}}>Criar Conta</SubmitButton>
+          <SubmitButton onPress={handleSubmit}>Criar Conta</SubmitButton>
         </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignText>Já tenho conta!</SignText>
+        </SignLink>
       </Container>
     </Background>
   );
