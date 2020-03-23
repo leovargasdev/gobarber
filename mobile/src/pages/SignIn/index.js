dee4ee26d95a5a5cfd9b6da52b8a2e1bfde76f61
Leonardo Vargas <leu1607@hotmail.com>
2020-03-22 22:58:01 -0300

    Adicionando as páginas de Cadastro e login de usuário

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
 
-// import Background from '~/components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignText,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({navigation}) {
   return (
-    // <Background>
-    <Text>SignIn</Text>
-    // </Background>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignText>Criar conta gratuita</SignText>
+        </SignLink>
+      </Container>
+    </Background>
   );
 }
--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {Container, Form, FormInput, SubmitButton} from './styles';
 
-export default function SignUp() {
+export default function SignUp({navigation}) {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome Completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Criar Conta</SubmitButton>
+        </Form>
+      </Container>
+    </Background>
   );
 }
